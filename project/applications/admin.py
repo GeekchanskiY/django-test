152e3a7506ec0bc38fab98cfbbdac27b23fb0e0a
geekchanskiy <astrovskiy.04@gmail.com>
2025-10-28 20:38:18 +0300

    admin set application admin model attrs

--- project/applications/admin.py
+++ project/applications/admin.py
@@ -5,6 +5,18 @@ from .models import Application
 
 @admin.register(Application)
 class ApplicationAdmin(admin.ModelAdmin):
+    # fields = ('user', 'email', 'vacancy', 'want_salary', 'created_at',)
+    fieldsets = (
+        ('primary user info', {'fields': ('user', 'email')}),
+        ('other info', {'fields': ('vacancy', 'want_salary', 'created_at')}),
+    )
+
+    readonly_fields = ('created_at',)
+
+    list_display = ('user', 'vacancy', 'created_at', 'salary_diff')
+
+    def salary_diff(self, obj):
+        return obj.vacancy.salary - obj.want_salary
 
     class Meta:
         verbose_name = 'application'
