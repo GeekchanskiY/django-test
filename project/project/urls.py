db5b7b780024537fa557281d5db7ab99891b6839
geekchanskiy <astrovskiy.04@gmail.com>
2025-10-30 20:02:15 +0300

    user profile init

--- project/project/urls.py
+++ project/project/urls.py
@@ -28,4 +28,9 @@ urlpatterns = [
     path('applications/', include(app_patterns)),
     path('', include(vacancies_patterns)),
     path('users/', include(users_patterns)),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] 
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
